fb441f0dd5b6f18b09f96bdb4595993d70d58acb
Ehsan <ehsanzeinalinia@gmail.com>
2022-08-03 13:28:55 +0430

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RadioButton
+
+A description of this package.
---
+++ Tests/RadioButtonTests/RadioButtonTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import RadioButton
+
+final class RadioButtonTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(RadioButton().text, "Hello, World!")
+    }
+}
