1a743ba4943f549703d5fca76a94233636f4032a
Ehsan <ehsanzeinalinia@gmail.com>
2022-08-03 14:02:43 +0430

    first commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "RadioButton",
+    platforms: [
+        .iOS(.v13)
+   ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -16,11 +19,9 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RadioButton",
-            dependencies: []),
+            dependencies: ["SnapKit"]),
         .testTarget(
             name: "RadioButtonTests",
             dependencies: ["RadioButton"]),
